136ffb5e29a7c2b8eb88b8998d7aabd01eccb7a6
Sanatan Shrivastava <45680718+Sanatan-Shrivastava@users.noreply.github.com>
2023-09-17 08:53:10 +0000

    email unified, (floor, room) sent to localhost:5000

--- static/server.js
+++ static/server.js
@@ -1,5 +1,5 @@
 // Define the base URL where your Flask server is hosted
-const baseUrl = "https://f2f5-20-118-130-2.ngrok-free.app";
+const baseUrl = "https://dc47-20-106-75-84.ngrok-free.app";
 
 // Function to make GET requests to your Flask APIs
 async function fetchApi(endpoint) {
@@ -38,7 +38,6 @@ async function getNotificationData() {
 function createNotificationCard(notification) {
   const card = document.createElement("div");
   card.className = "alert alert-light";
-  console.log("printing notificartion");
   // Create content for the notification card using the JSON data
   const content = `
           Service needed for <strong>${notification.AssetType}</strong> on <strong>${notification.ServiceRequiredFrom}</strong><br>
@@ -58,16 +57,92 @@ const notificationsContainer = document.getElementById(
 async function getFutureSeriesData() {
   try {
     const futureData = await fetchApi("/getFutureSeries");
-    document.getElementById("showRequestBtn").style.display='None';
+    document.getElementById("showRequestBtn").style.display = "None";
     console.log("Future Series Data:", futureData);
-    // Loop through the hard-coded JSON data and create notification cards
+
+    // Accumulate notifications
+    const notifications = [];
+
     futureData.futureServices.forEach((notification) => {
       const notificationCard = createNotificationCard(notification);
       notificationsContainer.appendChild(notificationCard);
+      notifications.push(notificationCard.innerHTML);
     });
+
+    // Combine all notifications into a single email message
+    const emailMessage = notifications.join("<br>");
+
+    emailjs.init("DAZHOYfCb-cRmDSIu");
+
+    // Send an email using EmailJS
+    emailjs
+      .send("service_iojgk85", "template_qsu5igb", {
+        to_email: "sanatan.mscs@gmail.com",
+        subject: "Service Required Notification",
+        message: emailMessage,
+      })
+      .then(
+        function (response) {
+          console.log("Email sent successfully:", response);
+        },
+        function (error) {
+          console.error("Email send error:", error);
+        }
+      );
   } catch (error) {
     console.error("Error:", error);
     throw error;
   }
 }
+
+
+var room;
+var floor;
+
+ // JavaScript for the first dropdown
+  const dropdownButton = document.getElementById("dropdownButton");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+
+  dropdownMenu.addEventListener("click", (e) => {
+    if (e.target.tagName === "OPTION") {
+      floor = e.target.value;
+      console.log("Selected value:", floor);
+    }
+  });
+
+// JavaScript for the second dropdown
+  const dropdownButton1 = document.getElementById("dropdownButton1");
+  const dropdownMenu1 = document.getElementById("dropdownMenu1");
+
+  dropdownMenu1.addEventListener("click", (e) => {
+    if (e.target.tagName === "OPTION") {
+      room = e.target.value;
+      console.log("Selected value:", room);
+    }
+  });
+
+
+
+function getRoomData() {
+  const payload = {
+    floor: floor,
+    room: room
+  }
+
+  console.log("sending payload to flask backend", payload)
+
+  fetch("http://localhost:5000/getRoomData", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log("Response from server:", data), arrayShow())
+
+    .catch((error) => console.error("Error:", error));
+}
+
+
 
